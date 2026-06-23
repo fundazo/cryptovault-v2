254e44ff1f0f1370750d68b4297e7358af8c6475
Fundazo <onakalueo@gmail.com>
2026-06-23 23:03:41 +0100

    Update email.js

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -3,8 +3,12 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: true,
+    port: process.env.SMTP_PORT || 587,
+    secure: true, // true for 465, false for other ports
+    requireTLS: true,
+    tls: {
+      rejectUnauthorized: false, // Allow self-signed certificates
+    },
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,

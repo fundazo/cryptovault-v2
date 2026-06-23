0d13435e005fdc0f9b8fce575ca282fac0604249
Fundazo <onakalueo@gmail.com>
2026-06-23 20:59:16 +0100

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_URL || '/api/v1';
-console.log('API URL:', process.env.REACT_APP_API_URL);
 
 const api = axios.create({
   baseURL: API_BASE,

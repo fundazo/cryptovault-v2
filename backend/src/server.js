ed990e9d9dea13102a3f936435a0bf5e90ee0661
Fundazo <onakalueo@gmail.com>
2026-06-24 00:49:08 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 const routes = require('./routes');
 
 const app = express();
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // CORS
